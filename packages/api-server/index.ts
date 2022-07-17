77e82baa7ccbdb4ac0020c0d7e894201659d8059
alousilva <lousilva@gmail.com>
2022-07-17 21:50:22 +0200

    Add trpc/server

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,7 +1,28 @@
 import express from 'express';
+import * as trpc from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import cors from 'cors';
+import { z } from 'zod';
+
+const appRouter = trpc.router().query('hello', {
+  resolve() {
+    return 'Greetings from /trpc/hello :)';
+  },
+});
 
 const app = express();
+app.use(cors());
 const port = 8080;
+
+export type AppRouter = typeof appRouter;
+
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: () => null,
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello from api-server');
